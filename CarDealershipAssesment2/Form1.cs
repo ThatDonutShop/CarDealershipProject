f09d652442cd2d2c3036da25e405a50a6cf55c22
ThatDonutShop <ollie.foose@gmail.com>
2023-05-16 21:01:11 +1200

    added sales

--- CarDealershipAssesment2/Form1.cs
+++ CarDealershipAssesment2/Form1.cs
@@ -7,15 +7,50 @@ namespace CarDealershipAssesment2
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void AddToList_Click(object sender, EventArgs e)
         {
+            // TODO: validation 
+
             var year = int.Parse(Year.Text);
             var price = decimal.Parse(Price.Text);
             var car = Car.Create(Make.Text, Model.Text, year, price);
+
             CarList.Items.Add(car);
+
+            ShowSaleStatistics();
+            
+            ClearInputs();
+        }
+
+        private void ClearInputs()
+        {
+            Make.Text = string.Empty;
+            Model.Text = string.Empty;
+            Year.Text = string.Empty;
+            Price.Text = string.Empty;
+
+            Make.Focus();
+        }
+
+        private void ShowSaleStatistics()
+        {
+            var cars = CarList.Items.OfType<Car>();
+
+            AverageCarSalesIncludingGst.Text = CarSales.GetAverageCarSalePriceIncludingGst(cars).ToString();
+        }
+
+        private void ClearCarList_Click(object sender, EventArgs e)
+        {
+            CarList.Items.Clear();
+
+            AverageCarSalesIncludingGst.Text = string.Empty;
+        }
+
+        private void LoadForm(object sender, EventArgs e)
+        {
+            ActiveControl = Make;
         }
     }
 }
