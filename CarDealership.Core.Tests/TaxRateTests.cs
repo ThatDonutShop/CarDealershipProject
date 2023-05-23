54fa42b663693f429801775d91a6cdab4ea98b32
ThatDonutShop <ollie.foose@gmail.com>
2023-05-23 19:44:20 +1200

    FIXED TESTT

--- CarDealership.Core.Tests/TaxRateTests.cs
+++ CarDealership.Core.Tests/TaxRateTests.cs
@@ -7,6 +7,7 @@ namespace CarDealership.Core.Tests
         [InlineData(0, 0, 0)]
         [InlineData(0, 19999, 0)]
         [InlineData(1, 19999, 0.17)]
+        [InlineData(1, 25000, 0.17)]
         [InlineData(1, 34999, 0.19)]
         [InlineData(80000, .01, .30)] // if greater than the highest bracked return highest rate.
         public void TaxRateCalculatedCorrect(decimal hondaPrice, decimal toyotaPrice, double expectedTaxRate)
--- CarDealership.Core/TaxRates.cs
+++ CarDealership.Core/TaxRates.cs
@@ -5,14 +5,24 @@ namespace CarDealership.Core
     {
         public static double Search(IEnumerable<Car> cars)
         {
+            decimal totalPrice = decimal.Zero;
+            
+            foreach (var car in cars)
+            {
+                totalPrice += car.Price;
+            }
+
+            return TaxRateSearching(totalPrice);
+        }
+
+        private static double TaxRateSearching(decimal totalPrice)
+        {
             double[] taxRates = { 0.0, 0.17, 0.19, 0.21, 0.25, 0.30 };
             decimal[] priceRanges = { 20000m, 35000m, 45000m, 60000m, 80000m };
 
-            var totalPrice = cars.Select(car => car.Price).Sum();
-
-            for (int i = 1; i < priceRanges.Length; i++)
+            for (int i = 0; i < priceRanges.Length; i++)
             {
-                if (totalPrice <= priceRanges[i])
+                if (totalPrice < priceRanges[i])
                 {
                     return taxRates[i];
                 }
